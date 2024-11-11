6ba69cc25a39cc419682918592d45a73798294b9
Andri Purnomo <andri.adrp@gmail.com>
2024-11-11 20:37:43 +0700

    remix contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,54 @@
 import {
   Form,
+  json,
   Links,
   Meta,
+  NavLink,
+  Outlet,
+  redirect,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from '@remix-run/react';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
+// existing imports
+
+import appStylesHref from './app.css?url';
+import { createEmptyContact, getContacts } from './data';
+import { useEffect, useState } from 'react';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: appStylesHref },
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const [query, setQuery] = useState(q || '');
+
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
+  useEffect(() => {
+    setQuery(q || '');
+  }, [q]);
+
   return (
     <html lang="en">
       <head>
@@ -19,32 +61,70 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              onChange={(e) => {
+                const isFirstSearch = q === null;
+                submit(e.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              id="search-form"
+              role="search"
+            >
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className={searching ? 'loading' : ''}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div
+          className={
+            navigation.state === 'loading' && !searching ? 'loading' : ''
+          }
+          id="detail"
+        >
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
